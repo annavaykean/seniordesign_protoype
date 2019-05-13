5aaecab0b60b23ad2521c6dc09096bedd18a07ec
annavaykean <42591977+annavaykean@users.noreply.github.com>
2019-05-13 14:19:28 -0400

    can scan for bluetooth device, working on display

--- sd_proto/lib/code/bluetooth_test_screen.dart
+++ sd_proto/lib/code/bluetooth_test_screen.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'dart:async';
 
-class BluetoothTestScreen extends StatelessWidget {
+class BluetoothTestScreen extends StatefulWidget {
+  @override
+  BluetoothState createState() => BluetoothState();
+}
+class BluetoothState extends State<BluetoothTestScreen> {
+
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  StreamSubscription scanner;
+  Map<DeviceIdentifier, ScanResult> scanResults = new Map();
+  List<BluetoothDevice> deviceList = [];
+  BluetoothDevice device;
+  bool connected = false;
+  bool pressed = false; //am I using this?
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -11,10 +26,51 @@ class BluetoothTestScreen extends StatelessWidget {
             children: <Widget> [
               RaisedButton(
                 child: const Text('Search for devices'),
-                onPressed: null,
+                onPressed: () => scanForDevices(),
+              ),
+              Column (
+                children: <Widget>[
+                  listDevices()
+                ]
               )
             ]
         )
     );
+  }
+
+  scanForDevices() {
+    print('INFO: Scanning for bluetooth devcies...');
+    scanner = flutterBlue.scan(timeout: const Duration(seconds: 5)).listen((scanResult){
+      print('INFO: deviceID = ${scanResult.device.id}');
+      deviceList.add(scanResult.device);
+      setState((){
+        scanResults[scanResult.device.id] = scanResult;
+      });
+    }, onDone: stopScanning());
+
+  }
+  stopScanning() {
+    if(scanner == null){
+      print("INFO: Attempted to cancel the scanner, but it was null");
+    } else {
+      scanner?.cancel();
+      scanner = null;
+    }
+    listDevices();
+  }
+
+  Widget listDevices() {
+    List<Widget> list = new List<Widget>();
+    print('INFO: scanResults length = ' + scanResults.length.toString());
+    for(var i=0;i<scanResults.length; i++){
+      list.add(new Text(scanResults[i].toString()));
+      if(scanResults[i] != null) {
+        print('INFO: SR = ' + scanResults[i].device.id.toString());
+      }
+    }
+    if(list == null){
+      print('INFO: the list for display is null :(');
+    }
+    return new Row(children: list);
   }
 }
