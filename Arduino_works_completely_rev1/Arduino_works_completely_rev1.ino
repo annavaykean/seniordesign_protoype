909b62c74c262f3ab1763e0aa30c86b0cca54901
bramjas <49532258+bramjas@users.noreply.github.com>
2019-07-14 18:40:58 -0400

    Update Arduino_works_completely_rev1.ino
    changes to tx3/rx3 works on pcb

--- Arduino_works_completely_rev1/Arduino_works_completely_rev1.ino
+++ Arduino_works_completely_rev1/Arduino_works_completely_rev1.ino
@@ -2,31 +2,29 @@
 
 #include <SoftwareSerial.h>
 #include <String.h>
-SoftwareSerial s(5,6);
-
+SoftwareSerial s(14,15);  //pins 3 and 4 on rev2 pcb change to 5 6 on rev3
 
 //****************************
-
 long count = millis();
-long timer = millis();
 
 //define variables
-
+//Large Sensors
 volatile int FR ;
 volatile int FL ;
 volatile int BR ;
 volatile int BL ;
 
-//small sensors
-
+//Small Sensors
 volatile int SF ;
 volatile int SB ;
 volatile int SL ;
 volatile int SR ;
 
-// x & y points
-//measured values on chair if center is assumed to be zero--> will change once sensors permanently mounted
 
+volatile int userUp = 0;
+// x & y points
+
+//measured values on chair if center is assumed to be zero--> will change once sensors permanently mounted
 
 
 /*Sensor Scheme
@@ -64,8 +62,6 @@ interrupts(); //enable all interrupts
   
 Serial.begin(9600);
 
-//pinMode(3,OUTPUT);
-//pinMode(11,OUTPUT);
   }
 
 
@@ -73,14 +69,14 @@ Serial.begin(9600);
 //timer compare interrupt service routine
 ISR(TIMER1_COMPA_vect){
 
- signed int x1= 5;
- signed int x2= -5.5;
- signed int x3= -2;
- signed int x4= 1.5;
- signed int x5= 0; 
- signed int x6= 0;
- signed int x7= 0; 
- signed int x8= 0;
+signed int x1= 5;
+signed int x2= -5.5;
+signed int x3= -2;
+signed int x4= 1.5;
+signed int x5= 0; 
+signed int x6= 0;
+signed int x7= 0; 
+signed int x8= 0;
 
 signed int y1= 2.5;
 signed int y2= 2.5;
@@ -93,41 +89,40 @@ signed int y8= 0;
 
 
   
-    char toSendX[6];
-    char toSendY[6];
-    signed int temp;
-    signed int number;
-    signed int x;
-    signed int y;
-    unsigned int vibeSet;
+char toSendX[6];
+char toSendY[6];
+signed int temp;
+signed int number;
+signed int x;
+signed int y;
+unsigned int vibeSet;
 signed int cogx = ((x1*(FR)+x2*(FL)+x3*(BL)+x4*(BR))/(4));
 signed int cogy = ((y1*(FR)+y2*(FL)+y3*(BL)+y4*(BR))/(4));
 
 
 
-if (s.available()>0)
+if (s.available())    //check if serial is available
   {
-//    char inChar = s.read();
-//    Serial.print(inChar);
-        byte val = s.read();
-      Serial.println("RECIEVE");
-       volatile int turnOnLED = val - 48;
-      if (turnOnLED == 1)
+    Serial.print("Available");
+        byte val = s.read();    //read value from esp, store in val
+      Serial.println("RECIEVE");  //confirm code is running
+       volatile int turnOnLED = val - 48;   //offset by 48 to recieve 1 or 0
+      if (turnOnLED == 1) 
     {
       Serial.println("LED Turned ON");
-      digitalWrite(LED_BUILTIN, LOW);     // make bultin led ON
+      digitalWrite(LED_BUILTIN, HIGH);     // make bultin led ON
       
-    
-    pinMode(11, OUTPUT);            // make external led ON
-    analogWrite(11,0);
+    pinMode(2, OUTPUT);            // make external led ON
+    digitalWrite(12,LOW);
+    analogWrite(2,0);
     vibeSet = 1;
     }
   
     else if (turnOnLED == 0)
     {
       Serial.println("LED Turned OFF");
-      //digitalWrite(LED_BUILTIN, HIGH);    // make bultin led OFF
-     pinMode(11,INPUT);             // make external led OFF
+      digitalWrite(LED_BUILTIN, LOW);    // make bultin led OFF
+     pinMode(2,INPUT);             // make external led OFF
      vibeSet = 0;
   }
   
@@ -139,7 +134,7 @@ if (s.available()>0)
   }
 
  //leaning forward 
-  if(((FL)>=10)&&((FR)>=10)&&(count>=8854)){ //change back to 100
+  if(((FL)>=100)&&((FR)>=100)&&(count>=8854)){ //change back to 100
      
  
 Serial.println(FR);
@@ -147,23 +142,21 @@ Serial.println(FL);
 Serial.println(BR);
 Serial.println(BL);
 Serial.println(vibeSet);
-//analogWrite(11,0);
-  
+
+   userUp = 0; 
    x = cogx;
    y = cogy;
+   
    if(vibeSet == 1){
-    analogWrite(11,140);
+    analogWrite(2,140);
+    digitalWrite(12,HIGH);  //testing pcbrev2 without vibrate
     }
     else if(vibeSet == 0){
-      analogWrite(11,0);
+      analogWrite(2,0);
+      digitalWrite(12,LOW); //testing pcbrev2 without vibrate
       }
-//Serial.println("ATTEMPT");
 
-//analogWrite(11,150);    //turn on
-////for(int a = 0; a<=5000;a++){}
-////analogWrite(11,0);
-
-  
+ 
 if(x<0){
   toSendX[0] = '-';
   toSendX[4] = ';';
@@ -193,7 +186,6 @@ s.flush();    //clear buffer
 
               }
    Serial.println();
-
 
 
 
@@ -229,260 +221,255 @@ s.flush();    //clear buffer
   
 
 //  //leaning backward
-//
-//  
-//  else if(((BR)>=100) && ((BL)>=100)&& (count>=8854)){
-// 
-//   x = cogx;
-//   y = cogy;
-//
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//
-//  digitalWrite(3,HIGH);
-//  digitalWrite(4,HIGH);
-//  delay(1000);
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//
-//if(x<0){
-//  toSendX[0] = '-';
-//  toSendX[4] = ';';
-//  toSendX[5] = 'x';
-//  x *= -1;
-//}
-//
-// else{
-//    toSendX[0] = '0';
-//    toSendX[4] = ';';
-//    toSendX[5] = 'x';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendX[i]=(char)((x%10) + 48);
-//      x = x/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//     
-//    Serial.print("X: ");
-//  for(int i=0;i<=5;i++){
-//          
-//           Serial.print(toSendX[i]);
-//           s.write(toSendX[i]);
-//
-//              }
-//   Serial.println();
-//
-//
-//
-//
-//
-//if(y<0){
-//  toSendY[0] = '-';
-//  toSendY[4] = ';';
-//  toSendY[5] = 'y';
-//  y *= -1;
-//}
-//
-// else{
-//    toSendY[0] = '0';
-//    toSendY[4] = ';';
-//    toSendY[5] = 'y';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendY[i]=(char)((y%10) + 48);
-//      y = y/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//  
-//     Serial.print("Y: ");
-//  for(int i=0;i<=5;i++){
-//           Serial.print(toSendY[i]);
-//           s.write(toSendY[i]);
-//    }
-//    Serial.println();
-//
-//    count = 0;
-//    }
-//    
-//    //leaning left
-//      else if(((FL)>=100) && ((BL)>=100)&& (count>=8854)){
-//  
-//   
-//   x = cogx;
-//   y = cogy;
-//
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//
-//  digitalWrite(3,HIGH);
-//  digitalWrite(4,HIGH);
-//  delay(100);
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//  
-//
-//if(x<0){
-//  toSendX[0] = '-';
-//  toSendX[4] = ';';
-//  toSendX[5] = 'x';
-//  x *= -1;
-//}
-//
-// else{
-//    toSendX[0] = '0';
-//    toSendX[4] = ';';
-//    toSendX[5] = 'x';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendX[i]=(char)((x%10) + 48);
-//      x = x/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//     
-//    Serial.print("X: ");
-//  for(int i=0;i<=5;i++){
-//        //  delay(20000);
-//           Serial.print(toSendX[i]);
-//           s.write(toSendX[i]);
-//
-//              }
-//   Serial.println();
-//
-//
-//
-//
-//
-//if(y<0){
-//  toSendY[0] = '-';
-//  toSendY[4] = ';';
-//  toSendY[5] = 'y';
-//  y *= -1;
-//}
-//
-// else{
-//    toSendY[0] = '0';
-//    toSendY[4] = ';';
-//    toSendY[5] = 'y';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendY[i]=(char)((y%10) + 48);
-//      y = y/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//  
-//     Serial.print("Y: ");
-//  for(int i=0;i<=5;i++){
-//           Serial.print(toSendY[i]);
-//          
-//           s.write(toSendY[i]);
-//    }
-//    Serial.println();
-//         //      delay(20000);
-//
-//        count = 0;
-//  }
-//        
-//    //leaning right
-//    
-//    else if(((BR)>=100) && ((FR)>=100) &&(count>=8854)){
-//
-//  
-//   x = cogx;
-//   y = cogy;
-//
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//
-//  digitalWrite(3,HIGH);
-//  digitalWrite(4,HIGH);
-//  delay(100);
-//  digitalWrite(3,LOW);
-//  digitalWrite(4,LOW);
-//
-//if(x<0){
-//  toSendX[0] = '-';
-//  toSendX[4] = ';';
-//  toSendX[5] = 'x';
-//  x *= -1;
-//}
-//
-// else{
-//    toSendX[0] = '0';
-//    toSendX[4] = ';';
-//    toSendX[5] = 'x';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendX[i]=(char)((x%10) + 48);
-//      x = x/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//     
-//    Serial.print("X: ");
-//  for(int i=0;i<=5;i++){
-//        //  delay(20000);
-//           Serial.print(toSendX[i]);
-//           s.write(toSendX[i]);
-//
-//              }
-//   Serial.println();
-//
-//
-//
-//
-//
-//if(y<0){
-//  toSendY[0] = '-';
-//  toSendY[4] = ';';
-//  toSendY[5] = 'y';
-//  y *= -1;
-//}
-//
-// else{
-//    toSendY[0] = '0';
-//    toSendY[4] = ';';
-//    toSendY[5] = 'y';
-//  }
-//    for( int i=3 ; i>=1;i--){
-//      toSendY[i]=(char)((y%10) + 48);
-//      y = y/10;
-//    }
-//    
-//s.flush();    //clear buffer
-//     // Serial.println();
-// //     Serial.println(toSend);
-//  
-//     Serial.print("Y: ");
-//  for(int i=0;i<=5;i++){
-//           Serial.print(toSendY[i]);
-//          
-//           s.write(toSendY[i]);
-//    }
-//    Serial.println();
-//         
-//        count = 0;
-//  }
+ else if(((BR)>=100) && ((BL)>=100)&& (count>=8854)){
+Serial.println(FR);
+Serial.println(FL);
+Serial.println(BR);
+Serial.println(BL);
+Serial.println(vibeSet);
+
+
+   userUp = 0; 
+   x = cogx;
+   y = cogy;
+   if(vibeSet == 1){
+    analogWrite(11,140);
+    }
+    else if(vibeSet == 0){
+      analogWrite(11,0);
+      }
+
+ 
+if(x<0){
+  toSendX[0] = '-';
+  toSendX[4] = ';';
+  toSendX[5] = 'x';
+  x *= -1;
+}
+
+ else{
+    toSendX[0] = '0';
+    toSendX[4] = ';';
+    toSendX[5] = 'x';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendX[i]=(char)((x%10) + 48);
+      x = x/10;
+    }
+    
+s.flush();    //clear buffer
+     // Serial.println();
+ //     Serial.println(toSend);
+     
+    Serial.print("X: ");
+  for(int i=0;i<=5;i++){
+        
+           Serial.print(toSendX[i]);
+           s.write(toSendX[i]);
+
+              }
+   Serial.println();
+
+
+if(y<0){
+  toSendY[0] = '-';
+  toSendY[4] = ';';
+  toSendY[5] = 'y';
+  y *= -1;
+}
+
+ else{
+    toSendY[0] = '0';
+    toSendY[4] = ';';
+    toSendY[5] = 'y';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendY[i]=(char)((y%10) + 48);
+      y = y/10;
+    }
+    
+s.flush();    //clear buffer
   
-  
+     Serial.print("Y: ");
+  for(int i=0;i<=5;i++){
+           Serial.print(toSendY[i]);
+          
+           s.write(toSendY[i]);
+    }
+    Serial.println();
+        
+                count = 0;
   }
 
+//leaning left
 
+ else if(((FL)>=100) && ((BL)>=100)&& (count>=8854)){
+Serial.println(FR);
+Serial.println(FL);
+Serial.println(BR);
+Serial.println(BL);
+Serial.println(vibeSet);
+
+   userUp = 0; 
+   x = cogx;
+   y = cogy;
+   if(vibeSet == 1){
+    analogWrite(11,140);
+    }
+    else if(vibeSet == 0){
+      analogWrite(11,0);
+      }
+
+ 
+if(x<0){
+  toSendX[0] = '-';
+  toSendX[4] = ';';
+  toSendX[5] = 'x';
+  x *= -1;
+}
+
+ else{
+    toSendX[0] = '0';
+    toSendX[4] = ';';
+    toSendX[5] = 'x';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendX[i]=(char)((x%10) + 48);
+      x = x/10;
+    }
+    
+s.flush();    //clear buffer
+     // Serial.println();
+ //     Serial.println(toSend);
+     
+    Serial.print("X: ");
+  for(int i=0;i<=5;i++){
+        
+           Serial.print(toSendX[i]);
+           s.write(toSendX[i]);
+
+              }
+   Serial.println();
+
+
+
+if(y<0){
+  toSendY[0] = '-';
+  toSendY[4] = ';';
+  toSendY[5] = 'y';
+  y *= -1;
+}
+
+ else{
+    toSendY[0] = '0';
+    toSendY[4] = ';';
+    toSendY[5] = 'y';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendY[i]=(char)((y%10) + 48);
+      y = y/10;
+    }
+    
+s.flush();    //clear buffer
   
-void loop() {
+     Serial.print("Y: ");
+  for(int i=0;i<=5;i++){
+           Serial.print(toSendY[i]);
+          
+           s.write(toSendY[i]);
+    }
+    Serial.println();
+        
+                count = 0;
+  }
+  
+          //leaning right  
+ else if(((BR)>=100) && ((FR)>=100) &&(count>=8854)){
+Serial.println(FR);
+Serial.println(FL);
+Serial.println(BR);
+Serial.println(BL);
+Serial.println(vibeSet);
+  
+   x = cogx;
+   y = cogy;
+   if(vibeSet == 1){
+    analogWrite(11,140);
+    }
+    else if(vibeSet == 0){
+      analogWrite(11,0);
+      }
+
+ 
+if(x<0){
+  toSendX[0] = '-';
+  toSendX[4] = ';';
+  toSendX[5] = 'x';
+  x *= -1;
+}
+
+ else{
+    toSendX[0] = '0';
+    toSendX[4] = ';';
+    toSendX[5] = 'x';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendX[i]=(char)((x%10) + 48);
+      x = x/10;
+    }
+    
+s.flush();    //clear buffer
+   
+    Serial.print("X: ");
+  for(int i=0;i<=5;i++){
+        
+           Serial.print(toSendX[i]);
+           s.write(toSendX[i]);
+
+              }
+   Serial.println();
+
+
+if(y<0){
+  toSendY[0] = '-';
+  toSendY[4] = ';';
+  toSendY[5] = 'y';
+  y *= -1;
+}
+
+ else{
+    toSendY[0] = '0';
+    toSendY[4] = ';';
+    toSendY[5] = 'y';
+  }
+    for( int i=3 ; i>=1;i--){
+      toSendY[i]=(char)((y%10) + 48);
+      y = y/10;
+    }
+    
+s.flush();    //clear buffer
+  
+     Serial.print("Y: ");
+  for(int i=0;i<=5;i++){
+           Serial.print(toSendY[i]);
+          
+           s.write(toSendY[i]);
+    }
+    Serial.println();
+        
+                count = 0;
+  }
+
+ else if((((FR)<20)&&((FL)<20)&&((BR)<20)&&((BL)<20))){
+   userUp = 1;
+   s.write('*');
+   userUp = 0;   
+    } 
+
+    
+    }
+
+ void loop() {
  FR = analogRead(A0);
  FL = analogRead(A1);
  BR = analogRead(A2);
@@ -494,27 +481,15 @@ void loop() {
  SB = analogRead(A5);
  SL = analogRead(A6);
  SR = analogRead(A7);
+ 
+//USE TO SEE IF SENSORS ARE OUTPUTTING AS EXPECTED
 
- pinMode(3,OUTPUT);
-pinMode(11,OUTPUT);
+//Serial.println(FL);
+//Serial.println(FR);
+//Serial.println(BL);
+//Serial.println(BR);
 
-
-
-
-//    s.read(turnOnLED);
-  
-//     End of transfer data statement
-  
-//  s.print(Serial.readString());
-  
-
-
-
-
-
-
-
-if((((FR)>=10)|((FL)>=10)|((BR)>=10)|((BL)>=10))){  //CHANGE back to 100 from 10
+if((((FR)>=20)|((FL)>=20)|((BR)>=20)|((BL)>=20))){  //CHANGE back to 100 from 10
   count ++;
   
    
@@ -524,5 +499,10 @@ if((((FR)>=10)|((FL)>=10)|((BR)>=10)|((BL)>=10))){  //CHANGE back to 100 from 10
       
 
  }
+
+// if(userUp == 1){
+// s.write('*');
+// userUp == 0; 
+//  }
 
 }
